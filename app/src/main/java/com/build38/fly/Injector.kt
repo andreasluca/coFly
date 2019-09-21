14b301b7f6f0508c668aae7233f0b948d5689be0
andreas <andreas.luca95@gmail.com>
2019-09-21 16:07:38 +0200

    Call works

--- app/src/main/java/com/build38/fly/Injector.kt
+++ app/src/main/java/com/build38/fly/Injector.kt
@@ -2,6 +2,7 @@ package com.build38.fly
 
 import android.content.Context
 import com.build38.fly.repository.AmadeusApi
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,6 +12,22 @@ class Injector(private val context: Context) {
         return Retrofit.Builder().apply {
             baseUrl(Constants.AMADEUS_BASE_URL)
             addConverterFactory(GsonConverterFactory.create())
+            client(injectOkHttpClient())
+        }.build()
+    }
+
+    //buildConfigField "String" "API_KEY" "\"QYlNY5V2OWQ4R2E1JfDD76PGHetJvT7N\""
+    //            buildConfigField "String" "API_SECRET" "\"2AMwnKeBumrIcuX6\""
+
+    private fun injectOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            addInterceptor { chain ->
+                val request = chain.request().newBuilder().apply {
+                    addHeader("cache-control", "no-cache")
+                    addHeader("Authorization", "Bearer Y8qOVjzBoIV3Jx0aBgaVDkcc3Lli")
+                }.build()
+                chain.proceed(request)
+            }
         }.build()
     }
 
--- app/src/main/java/com/build38/fly/MainActivity.kt
+++ app/src/main/java/com/build38/fly/MainActivity.kt
@@ -3,7 +3,9 @@ package com.build38.fly
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.build38.fly.model.amadeus.AmadeusOfferItem
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,21 +16,27 @@ class MainActivity : AppCompatActivity() {
         private const val LOG_TAG = "AMADEUS"
     }
 
+    private lateinit var retrofitCall: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val amadeusApi = Injector(this).provideAmadeusApi()
-        amadeusApi
-            .getRoundTripFlights("BCN", "MAD", "2019-10-01", "2019-11-01")
-            .enqueue(object : Callback<List<AmadeusOfferItem>> {
-                override fun onResponse(call: Call<List<AmadeusOfferItem>>, response: Response<List<AmadeusOfferItem>>) {
-                    Log.d(LOG_TAG, "I AM ALIVE")
-                }
+        retrofitCall = searchButton
 
-                override fun onFailure(call: Call<List<AmadeusOfferItem>>, t: Throwable) {
-                    Log.d(LOG_TAG, ":((((((((((")
-                }
-        })
+        retrofitCall.setOnClickListener {
+            val amadeusApi = Injector(this).provideAmadeusApi()
+            amadeusApi
+                .getRoundTripFlights("BCN", "MAD", "2019-10-01", "2019-11-01")
+                .enqueue(object : Callback<List<AmadeusOfferItem>> {
+                    override fun onResponse(call: Call<List<AmadeusOfferItem>>, response: Response<List<AmadeusOfferItem>>) {
+                        Log.d(LOG_TAG, "I AM ALIVE")
+                    }
+
+                    override fun onFailure(call: Call<List<AmadeusOfferItem>>, t: Throwable) {
+                        Log.d(LOG_TAG, ":((((((((((")
+                    }
+                })
+        }
     }
 }
