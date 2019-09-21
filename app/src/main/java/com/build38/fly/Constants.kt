156c4e1a8cd86674b47b02ccff37189fdc9f61e8
andreas <andreas.luca95@gmail.com>
2019-09-21 14:39:40 +0200

    Fix URL

--- app/src/main/java/com/build38/fly/Constants.kt
+++ app/src/main/java/com/build38/fly/Constants.kt
@@ -2,6 +2,6 @@ package com.build38.fly
 
 class Constants {
     companion object {
-        const val AMADEUS_BASE_URL = "http://test.api.amadeus.com/v1/shopping/"
+        const val AMADEUS_BASE_URL = "https://test.api.amadeus.com/v1/shopping/"
     }
 }
