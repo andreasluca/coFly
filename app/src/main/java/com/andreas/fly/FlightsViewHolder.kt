315a8d4463a8bf575a516f78a4348505d9bae2ed
Andreas Luca <andreas.luca95@gmail.com>
2019-10-30 00:02:45 +0100

    PoC working

--- app/src/main/java/com/andreas/fly/FlightsViewHolder.kt
+++ app/src/main/java/com/andreas/fly/FlightsViewHolder.kt
@@ -2,6 +2,14 @@ package com.andreas.fly
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.flights_view_holder.view.*
 
 class FlightsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val depature = itemView.departureTextId
+    val depatureTime = itemView.depatureTimeTextId
+    val arrival = itemView.arrivalTextId
+    val arrivalTime = itemView.arrivalTimeTextId
+    val price = itemView.priceTextId
+    val duration = itemView.durationTextId
 }
