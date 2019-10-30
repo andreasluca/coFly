9c24e9d01a48874c80097b6b5a35a05249e5502f
Andreas Luca <andreas.luca95@gmail.com>
2019-10-30 01:21:52 +0100

    Add date picker

--- app/src/main/java/com/andreas/fly/MainActivity.kt
+++ app/src/main/java/com/andreas/fly/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.andreas.fly
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,9 @@ import com.andreas.fly.model.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity(), SearchFlightsViewInterface {
@@ -47,6 +51,43 @@ class MainActivity : AppCompatActivity(), SearchFlightsViewInterface {
         val passenger4 = Passenger("Marc", "CDG")
         val brothers = arrayOf(passenger1, passenger2, passenger3, passenger4)
         val travelGroup1 = TravelGroup("Brothers", brothers)
+
+        val cal = Calendar.getInstance()
+
+        val departureSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "yyyy-MM-dd"
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            departureDateId.text = sdf.format(cal.time)
+        }
+
+        val arrivalSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "yyyy-MM-dd"
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            arrivalDateId.text = sdf.format(cal.time)
+
+        }
+
+        departureDateId.setOnClickListener {
+            DatePickerDialog(this@MainActivity, departureSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        arrivalDateId.setOnClickListener {
+            DatePickerDialog(this@MainActivity, arrivalSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
         retrofitCall.setOnClickListener {
             GlobalScope.launch {
